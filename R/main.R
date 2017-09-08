288e66eb898eb931a4fb3088cf7dbd8d53c467ca
Andrew Thomas Jones <andrewthomasjones@gmail.com>
2017-09-08 14:41:54 +1000

    .

--- R/main.R
+++ R/main.R
@@ -1,6 +1,6 @@
 
-# filname='/home/andrew/projects/EMMIX-GENE/dat/golub_norm.dat'
-#/home/andrew/projects/EMMIX-GENE/dat/alon_norm.dat
+# filename='/home/andrew/projects/EMMIX-GENE/dat/golub_norm.dat'
+# filename='/home/andrew/projects/EMMIX-GENE/dat/alon_norm.dat'
 
 #' Selects Genes from data supplied.
 #'
@@ -14,7 +14,7 @@ select_genes<-function(filename, random_starts=4, max_it = 400, ll_thresh = 8, m
     data<-as.matrix(read.delim(filename, sep=" ", header=F))
     a<-emmix_gene(data)
     genes <- data[a$g>1,]
-    return(genes)
+    return(a)
 }
 
 #' Clusters genes
@@ -24,7 +24,18 @@ select_genes<-function(filename, random_starts=4, max_it = 400, ll_thresh = 8, m
 #' @return mclust object
 #' @examples
 cluster_genes<-function(genes, g=NULL){
-  clust_genes<-Mclust(genes, G=g, modelNames = "VII")
+  clust_genes<-Mclust((genes), G=g, modelNames = "VII")
+  g<-clust_genes$G
+  ll_rank_stat<-array(0,g)
+  
+  for(i in 1:g){
+    ll_rank_stat[i]<-each_gene(colMeans(clust_genes$data[clust_genes$classification==i,]))$Ratio
+  }
+  
+  tmp<-factor(clust_genes$classification)
+  levels(tmp) <- as.character(order(unlist(ll_rank_stat)))
+  clust_genes$classification<-as.numeric(as.character(tmp))
+  
   return(clust_genes)
 }
 
@@ -35,7 +46,17 @@ cluster_genes<-function(genes, g=NULL){
 #' @return ?
 #' @examples
 cluster_tissues<-function(genes, method='t'){
+  #clust_genes$data[clust_genes$classification==1,]
+  g<-clust_genes$G
+  p<-ncol(clust_genes$data)
+  clustering<-array(0,c(g,p))
+  
   if(method=='t'){
+    for(i in 1:g){
+        group_means <- colMeans(data[which(a$g>1)[clust_genes$classification==i],])
+        t_fit<-emmix_t(group_means, 2, 100)
+        clustering[i,]<-as.numeric(xor(t_fit$Clusters, (t_fit$mu[1]>t_fit$mu[2])))
+     }
     
   }
   if(method=='mfa')
@@ -50,9 +71,6 @@ cluster_tissues<-function(genes, method='t'){
 heat_maps<-function(clust_genes){
 
 }
-
-
-
 
 
 
