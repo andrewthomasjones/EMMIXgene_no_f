88468647582e2b2547e24fda14c9a1092be529ec
Andrew Thomas Jones <andrewthomasjones@gmail.com>
2017-09-15 20:15:27 +1000

    pretty much works now

--- R/main.R
+++ R/main.R
@@ -73,6 +73,7 @@ cluster_genes<-function(gen, g=NULL){
   
   for(i in 1:g){
     ll_rank_stat[i]<-each_gene(colMeans(clust_genes$data[clust_genes$classification==i,]))$Ratio
+    
   }
   
   #reorder clusters as ranked by mean of ll statistic from emmix-gene fit
@@ -86,39 +87,73 @@ cluster_genes<-function(gen, g=NULL){
 
 #' Clusters tissues
 #'
-#' @param genes matrix of genes
-#' @param g number of clusters
-#' @return ?
+#' @param gen emmix-gene object
+#' @param clusters mclust object
+#' @param method Method for seperating tissue classes. Can be either 't' for a univariate mixture of t-distributions on gene cluster means, or 'mfa' for a mixture of factor analysers. 
+#' @return a clustering for each sample (columns) by each group(rows)
 #' @examples
 #' 
 #' @export
-cluster_tissues<-function(genes, method='t'){
-  #clust_genes$data[clust_genes$classification==1,]
-  g<-clust_genes$G
-  p<-ncol(clust_genes$data)
+cluster_tissues<-function(gen, clusters, method='t'){
+  
+  g<-clusters$G
+  p<-ncol(clusters$data)
   clustering<-array(0,c(g,p))
+  clustering2<-array(0,c(g,p))
   
   if(method=='t'){
     for(i in 1:g){
-        group_means <- colMeans(data[which(a$g>1)[clust_genes$classification==i],])
-        t_fit<-emmix_t(group_means, 2, 100)
+        group_means <- colMeans(gen$genes[clusters$classification==i,])
+        t_fit<-emmix_t(group_means, 2)
         clustering[i,]<-as.numeric(xor(t_fit$Clusters, (t_fit$mu[1]>t_fit$mu[2])))
      }
     
   }
-  if(method=='mfa')
-  return()
+  
+  if(method=='mfa'){
+    for(i in 1:g){
+      group <- (gen$genes[clusters$classification==i,])
+      #actually mixture of common factor analysers. consider fixing.
+      mfa_fit<-mcfa(t(group), 2, 2)
+      clustering[i,]<- as.numeric(xor(predict(mfa_fit, t(group))-1, (diff(mfa_fit$xi[1,])>0))) 
+    }
+    
+    
+  }
+  
+  return(clustering)
 }
 
 #' Heat maps
 #'
-#' @param clust_genes mclust object
-#' @return ?
+#' @param clust_genes matrix of genes
+#' @return 
 #' @examples
 #' 
 #' @export
-heat_maps<-function(clust_genes){
-
+heat_maps<-function(clust_genes, clustering=NULL){
+  colnames(clust_genes) <- NULL
+  if(!is.null(clustering)){
+    clust_genes<-clust_genes[,order(clustering)]
+  }
+  
+  df_heatmap<-melt(clust_genes)
+  names(df_heatmap)<-c("genes", "samples",  "expression_level")
+  df_heatmap$genes<-factor(df_heatmap$genes)
+  df_heatmap$samples<-factor(df_heatmap$samples)
+  
+  plot<-ggplot(df_heatmap, aes(samples,genes )) + geom_tile(aes(fill = expression_level),  color = "white") +
+    scale_fill_distiller(palette = "Spectral", limits=c(-4,4)) + 
+    ylab("Genes") +
+    xlab("Samples") +
+    theme(legend.title = element_text(size = 10),
+          legend.text = element_text(size = 12),
+          plot.title = element_text(size=16),
+          axis.title=element_text(size=14,face="bold"),
+          axis.text.x = element_blank(), axis.text.y = element_blank(), axis.ticks.y=element_blank(),axis.ticks.x=element_blank()) +
+    labs(fill = "Expression level")
+  
+  return(plot)
 }
 
 
--- test_script.R
+++ test_script.R
@@ -1,10 +1,21 @@
 library(EMMIXgene)
-filename='/home/andrew/projects/EMMIX-GENE/dat/golub_norm.dat'
+filename_b='/home/andrew/projects/EMMIX-GENE/dat/golub_norm.dat'
 filename='/home/andrew/projects/EMMIX-GENE/dat/alon_norm.dat'
 
-test1<-select_genes(filename)
-test2<-cluster_genes(test1)
-test3<-cluster_tissues(test2)
-test4<-heat_maps(test2)
+g=10
 
+test1<-select_genes(filename)
+test2<-cluster_genes(test1, g)
+test3a<-cluster_tissues(test1, test2)
+
+test3<-cluster_tissues(test1, test2, 'mfa')
+
+
+for(k in 1:g){
+  test4<-heat_maps(test1$genes[test2$classification==k,], test3[k,]) 
+  plot(test4)
+  
+  test5<-heat_maps(test1$genes[test2$classification==k,]) 
+  plot(test5)
+}
 
